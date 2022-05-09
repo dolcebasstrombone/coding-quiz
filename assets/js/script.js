e341252fe9ec924dc0475cb42e49ada35934068c
dolcebasstrombone <dolcebasstrombone@gmail.com>
2022-05-09 16:01:44 -0400

    wrap code in startQuiz function, separate endQuiz function, more pseudocode

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,61 +43,65 @@ var questionsObj = [
     correct: "3. Quotes",
   },
 ];
-var timerEl = document.querySelector('#timer');
+var timerEl = document.querySelector("#timer");
 
 //------------------------------page with title and start button, that when clicked, starts the quiz.
 // get DOM reference for button
 var startQuizButton = document.querySelector("#start-quiz");
 //add event listener to button
 startQuizButton.addEventListener("click", startQuiz);
-//called by event listener
-var startQuiz = function () {
-  window.alert("you did it!");
-};
-///-----------------------------event listener not working correctly, alerts when page is loaded, whyyyyyyyyyyyyyy
+///-----------------------------------------------------------end code block of button with listener
 
-//starting the quiz starts a countdown. a question is shown with four clickable answers
-var timeLeft = 120;
-//--------------------------copied from activity, dont forget to make sure vars match up with my own code
-//stopping the timer and capturing it could be here or in another funtion
-var timer = setInterval(function () {
-  // if time left is more than 1, count down, text content shows plural
-  if (timeLeft > 1) {
-    timerEl.textContent = timeLeft + " seconds remaining";
-    timeLeft--;
-  } // if time left is one, count down, text content shows singular
-  else if (timeLeft === 1) {
-    timerEl.textContent = timeLeft + " second remaining";
-    timeLeft--;
-  } // if there is no time left, stop the counter
-  else { //--------------------------------------this is what might need to be changed
-    timerEl.textContent = "";
-    clearInterval(timer);
+//called by event listener (wasnt able to write as var, why?)
+function startQuiz() {
+  var timeLeft = 120;
+  //sets and displays timeLeft
+  var timer = setInterval(function () {
+    // if time left is more than 1, count down, text content shows plural
+    if (timeLeft > 1) {
+      timerEl.textContent = timeLeft + " seconds remaining";
+      timeLeft--;
+    } // if time left is one, count down, text content shows singular
+    else if (timeLeft === 1) {
+      timerEl.textContent = timeLeft + " second remaining";
+      timeLeft--;
+    } // if there is no time left, stop the counter
+    else {
+      //-------------------------------------------------------------this is what might need to be changed
+      timerEl.textContent = "";
+      clearInterval(timer);
+    } //-------------------------------------------------------------end suspect code block
+  }, 1000); //1000 represents milliseconds
+
+  //for as long as there are questions left, do this
+  for (var i = 0; i < questionsObj.length; i++) {
+    //if theres still time, show another question
+    if (timeLeft > 0) {
+      //display a question (the next question in array) (maybe later can mess with random question)
+      // after clicking an answer, the next question is displayed
+      var nextQuestion = function () {
+        //handles displaying the question. display next question in questionsObj
+        //dynamically display in html
+        //add correct class to correct, wrong class to wrong
+        //if answer is right or wrong
+        var checkAnswer = function () {
+          //if right display "correct" and move on
+          //else wrong display "wrong", deduct time, move on
+          //both displays will need to be appended dynamicaqlly in html
+          //return timeLeft
+        };
+        checkAnswer;
+      };
+      nextQuestion;
+      //break loop needed?
+    //if there isnt time or there are no more questions
+    } else {
+      //endQuiz (captures score)
+      //break loop?
+    }
   }
-//1000 represents milliseconds
-}, 1000);
-//-----------------------------end activity code
-
-//for loop, put inside start quiz function
-for (var i = 0; i < questionsObj.length; i++ /*&& as long as timeLeft > 0?*/) {
-  //display a question
-  // after clicking an answer, the next question is displayed
-  var nextQuestion = function () {
-    //handles displaying the question. display next question in questionsObj
-    //add correct class to correct, wrong class to wrong
-  };
-  //if answer is right or wrong
-  var checkAnswer = function () {
-    //if right display "correct" and move on
-    //else wrong display "wrong", move on, deduct time
-    //return timeLeft
-  };
 }
 
-// if the timer runs out, the game ends and the player is shown the submit score screen (inside for loop statement?)
-// run end quiz function, also called when/if questions run out
-//after all questions have been displayed, the time remaining is displayed as the score. the latest correct/wrong label is underneath
-// run end quiz function, also called when/if questions run out
 var endQuiz = function () {
   //capture time left as score
   var score = timeLeft;
@@ -106,5 +110,3 @@ var endQuiz = function () {
   //move to new html page that displays all scores highest to lowest
   window.location.pathname(highscore.html); //find out if this actually works, also try window.location.href = ""; or window.replace();
 };
-
-//from start to finish, a veiw high score link is available. clicking it shows highscores in local storage. same html page as in endQuiz
